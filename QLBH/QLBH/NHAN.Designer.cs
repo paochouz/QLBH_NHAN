1dd0dfbe1b7cc4703d985c922f49220a27e0f2a1
paochouz <nguyenlebaochau157@gmail.com>
2025-05-15 12:24:13 +0700

    Add files via upload

---
+++ QLBH/QLBH/NHAN.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace QLBH
+{
+    partial class NHAN
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            textBox_MaDDH = new TextBox();
+            textBox_MaNV = new TextBox();
+            textBox_MaNCC = new TextBox();
+            textBox_MaLH = new TextBox();
+            dateTimePicker_NgayNhan = new DateTimePicker();
+            dateTimePicker_ThgianNhan = new DateTimePicker();
+            checkBox_PTTTTienMat = new CheckBox();
+            checkBox_PTTTCKhoan = new CheckBox();
+            button_Them = new Button();
+            dataGridView_HoadonNhap = new DataGridView();
+            MaDDH = new DataGridViewTextBoxColumn();
+            MaNV = new DataGridViewTextBoxColumn();
+            MaNCC = new DataGridViewTextBoxColumn();
+            MaLH = new DataGridViewTextBoxColumn();
+            NgayNhan = new DataGridViewTextBoxColumn();
+            ThgianNhan = new DataGridViewTextBoxColumn();
+            PTTT = new DataGridViewTextBoxColumn();
+            button_Sua = new Button();
+            button_Xoa = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_HoadonNhap).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(302, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(491, 41);
+            label1.TabIndex = 1;
+            label1.Text = "QUẢN LÝ HÓA ĐƠN NHẬP HÀNG";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(34, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Mã DDH";
+            label2.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(34, 315);
+            label3.Name = "label3";         
+            label3.Size = new Size(119, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Ngày Nhận";
+            label3.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(34, 369);
+            label4.Name = "label4";
+            label4.Size = new Size(161, 28);
+            label4.TabIndex = 4;
+            label4.Text = "Thời Gian Nhận";
+            label4.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(358, 564);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 28);
+            label5.TabIndex = 5;
+            label5.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(34, 419);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 28);
+            label6.TabIndex = 6;
+            label6.Text = "PTTT\r\n";
+            label6.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(386, 227);
+            label9.Name = "label9";
+            label9.Size = new Size(0, 28);
+            label9.TabIndex = 9;
+            label9.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(34, 259);
+            label10.Name = "label10";
+            label10.Size = new Size(126, 28);
+            label10.TabIndex = 10;
+            label10.Text = "Mã Lô Hàng";
+            label10.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(34, 148);
+            label11.Name = "label11";
+            label11.Size = new Size(147, 28);
+            label11.TabIndex = 11;
+            label11.Text = "Mã Nhân Viên";
+            label11.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(34, 201);
+            label12.Name = "label12";
+            label12.Size = new Size(182, 28);
+            label12.TabIndex = 12;
+            label12.Text = "Mã Nhà Cung Cấp\r\n";
+            label12.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // textBox_MaDDH
+            // 
+            textBox_MaDDH.Location = new Point(288, 91);
+            textBox_MaDDH.Name = "textBox_MaDDH";
+            textBox_MaDDH.Size = new Size(307, 31);
+            textBox_MaDDH.TabIndex = 13;
+            // 
+            // textBox_MaNV
+            // 
+            textBox_MaNV.Location = new Point(288, 148);
+            textBox_MaNV.Name = "textBox_MaNV";
+            textBox_MaNV.Size = new Size(307, 31);
+            textBox_MaNV.TabIndex = 14;
+            // 
+            // textBox_MaNCC
+            // 
+            textBox_MaNCC.Location = new Point(288, 198);
+            textBox_MaNCC.Name = "textBox_MaNCC";
+            textBox_MaNCC.Size = new Size(307, 31);
+            textBox_MaNCC.TabIndex = 15;
+            // 
+            // textBox_MaLH
+            // 
+            textBox_MaLH.Location = new Point(288, 256);
+            textBox_MaLH.Name = "textBox_MaLH";
+            textBox_MaLH.Size = new Size(307, 31);
+            textBox_MaLH.TabIndex = 16;
+            // 
+            // dateTimePicker_NgayNhan
+            // 
+            dateTimePicker_NgayNhan.Format = DateTimePickerFormat.Short;
+            dateTimePicker_NgayNhan.Location = new Point(289, 314);
+            dateTimePicker_NgayNhan.Name = "dateTimePicker_NgayNhan";
+            dateTimePicker_NgayNhan.Size = new Size(145, 31);
+            dateTimePicker_NgayNhan.TabIndex = 17;
+            dateTimePicker_NgayNhan.Value = new DateTime(2025, 5, 12, 10, 56, 41, 0);
+            dateTimePicker_NgayNhan.Visible = false;
+            dateTimePicker_NgayNhan.ValueChanged += dateTimePicker_NgayNhan_ValueChanged;
+            // 
+            // dateTimePicker_ThgianNhan
+            // 
+            dateTimePicker_ThgianNhan.Format = DateTimePickerFormat.Time;
+            dateTimePicker_ThgianNhan.Location = new Point(289, 368);
+            dateTimePicker_ThgianNhan.Name = "dateTimePicker_ThgianNhan";
+            dateTimePicker_ThgianNhan.ShowUpDown = true;
+            dateTimePicker_ThgianNhan.Size = new Size(145, 31);
+            dateTimePicker_ThgianNhan.TabIndex = 18;
+            dateTimePicker_ThgianNhan.Value = new DateTime(2025, 5, 12, 0, 0, 0, 0);
+            dateTimePicker_ThgianNhan.ValueChanged += dateTimePicker_ThgianNhan_ValueChanged;
+            // 
+            // checkBox_PTTTTienMat
+            // 
+            checkBox_PTTTTienMat.AutoSize = true;
+            checkBox_PTTTTienMat.Location = new Point(288, 421);
+            checkBox_PTTTTienMat.Name = "checkBox_PTTTTienMat";
+            checkBox_PTTTTienMat.Size = new Size(102, 29);
+            checkBox_PTTTTienMat.TabIndex = 19;
+            checkBox_PTTTTienMat.Text = "Tiền mặt";
+            checkBox_PTTTTienMat.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_PTTTCKhoan
+            // 
+            checkBox_PTTTCKhoan.AutoSize = true;
+            checkBox_PTTTCKhoan.Location = new Point(288, 456);
+            checkBox_PTTTCKhoan.Name = "checkBox_PTTTCKhoan";
+            checkBox_PTTTCKhoan.Size = new Size(147, 29);
+            checkBox_PTTTCKhoan.TabIndex = 20;
+            checkBox_PTTTCKhoan.Text = "Chuyển khoản";
+            checkBox_PTTTCKhoan.UseVisualStyleBackColor = true;
+            // 
+            // button_Them
+            // 
+            button_Them.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_Them.Location = new Point(131, 507);
+            button_Them.Name = "button_Them";
+            button_Them.Size = new Size(110, 48);
+            button_Them.TabIndex = 21;
+            button_Them.Text = "Thêm";
+            button_Them.UseVisualStyleBackColor = true;
+            button_Them.Click += button_Them_Click;
+            // 
+            // dataGridView_HoadonNhap
+            // 
+            dataGridView_HoadonNhap.BackgroundColor = SystemColors.ControlLight;
+            dataGridView_HoadonNhap.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView_HoadonNhap.Columns.AddRange(new DataGridViewColumn[] { MaDDH, MaNV, MaNCC, MaLH, NgayNhan, ThgianNhan, PTTT });
+            dataGridView_HoadonNhap.Location = new Point(659, 74);
+            dataGridView_HoadonNhap.Name = "dataGridView_HoadonNhap";
+            dataGridView_HoadonNhap.RowHeadersWidth = 51;
+            dataGridView_HoadonNhap.Size = new Size(415, 411);
+            dataGridView_HoadonNhap.TabIndex = 23;
+            dataGridView_HoadonNhap.CellContentClick += dataGridView_HoadonNhap_CellClick;
+            // 
+            // MaDDH
+            // 
+            MaDDH.HeaderText = "Mã DDH";
+            MaDDH.MinimumWidth = 6;
+            MaDDH.Name = "MaDDH";
+            MaDDH.Width = 125;
+            // 
+            // MaNV
+            // 
+            MaNV.HeaderText = "Mã NV";
+            MaNV.MinimumWidth = 6;
+            MaNV.Name = "MaNV";
+            MaNV.Width = 125;
+            // 
+            // MaNCC
+            // 
+            MaNCC.HeaderText = "Mã NCC";
+            MaNCC.MinimumWidth = 6;
+            MaNCC.Name = "MaNCC";
+            MaNCC.Width = 125;
+            // 
+            // MaLH
+            // 
+            MaLH.HeaderText = "Mã LH";
+            MaLH.MinimumWidth = 6;
+            MaLH.Name = "MaLH";
+            MaLH.Width = 125;
+            // 
+            // NgayNhan
+            // 
+            NgayNhan.HeaderText = "Ngày Nhận";
+            NgayNhan.MinimumWidth = 6;
+            NgayNhan.Name = "NgayNhan";
+            NgayNhan.Width = 125;
+            // 
+            // ThgianNhan
+            // 
+            ThgianNhan.HeaderText = "Thời Gian Nhận";
+            ThgianNhan.MinimumWidth = 6;
+            ThgianNhan.Name = "ThgianNhan";
+            ThgianNhan.Width = 125;
+            // 
+            // PTTT
+            // 
+            PTTT.HeaderText = "PTTT";
+            PTTT.MinimumWidth = 6;
+            PTTT.Name = "PTTT";
+            PTTT.Width = 125;
+            // 
+            // button_Sua
+            // 
+            button_Sua.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_Sua.Location = new Point(314, 507);
+            button_Sua.Name = "button_Sua";
+            button_Sua.Size = new Size(110, 48);
+            button_Sua.TabIndex = 24;
+            button_Sua.Text = "Sửa";
+            button_Sua.UseVisualStyleBackColor = true;
+            button_Sua.Click += button_Sua_Click;
+            // 
+            // button_Xoa
+            // 
+            button_Xoa.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_Xoa.Location = new Point(496, 507);
+            button_Xoa.Name = "button_Xoa";
+            button_Xoa.Size = new Size(110, 48);
+            button_Xoa.TabIndex = 25;
+            button_Xoa.Text = "Xóa";
+            button_Xoa.UseVisualStyleBackColor = true;
+            button_Xoa.Click += button_Xoa_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1101, 598);
+            Controls.Add(button_Xoa);
+            Controls.Add(button_Sua);
+            Controls.Add(dataGridView_HoadonNhap);
+            Controls.Add(button_Them);
+            Controls.Add(checkBox_PTTTCKhoan);
+            Controls.Add(checkBox_PTTTTienMat);
+            Controls.Add(dateTimePicker_ThgianNhan);
+            Controls.Add(dateTimePicker_NgayNhan);
+            Controls.Add(textBox_MaLH);
+            Controls.Add(textBox_MaNCC);
+            Controls.Add(textBox_MaNV);
+            Controls.Add(textBox_MaDDH);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ImeMode = ImeMode.On;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView_HoadonNhap).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private TextBox textBox_MaDDH;
+        private TextBox textBox_MaNV;
+        private TextBox textBox_MaNCC;
+        private TextBox textBox_MaLH;
+        private DateTimePicker dateTimePicker_NgayNhan;
+        private DateTimePicker dateTimePicker_ThgianNhan;
+        private CheckBox checkBox_PTTTTienMat;
+        private CheckBox checkBox_PTTTCKhoan;
+        private Button button_Them;
+        private DataGridView dataGridView_HoadonNhap;
+        private DataGridViewTextBoxColumn MaDDH;
+        private DataGridViewTextBoxColumn MaNV;
+        private DataGridViewTextBoxColumn MaNCC;
+        private DataGridViewTextBoxColumn MaLH;
+        private DataGridViewTextBoxColumn NgayNhan;
+        private DataGridViewTextBoxColumn ThgianNhan;
+        private DataGridViewTextBoxColumn PTTT;
+        private Button button_Sua;
+        private Button button_Xoa;
+    }
+}
---
+++ QLBH/QLBH/NHAN.cs
@@ -0,0 +1,157 @@
+﻿namespace QLBH
+{
+    public partial class NHAN : Form
+    {
+        private int selectedRowIndex = -1;
+        public NHAN()
+        {
+            InitializeComponent();
+            dateTimePicker_NgayNhan.Value = DateTime.Now;
+            dateTimePicker_ThgianNhan.Value = DateTime.Now;
+            dateTimePicker_NgayNhan.Visible = true;
+            dateTimePicker_ThgianNhan.Visible = true;
+
+            // Liên kết sự kiện CheckedChanged
+            checkBox_PTTTTienMat.CheckedChanged += checkBox_PTTTTienMat_CheckedChanged;
+            checkBox_PTTTCKhoan.CheckedChanged += checkBox_PTTTCKhoan_CheckedChanged;
+
+            // Sự kiện CellClick để xử lý Sửa/Xóa
+            dataGridView_HoadonNhap.CellClick += dataGridView_HoadonNhap_CellClick;
+        }
+        private void checkBox_PTTTTienMat_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_PTTTTienMat.Checked)
+            {
+                checkBox_PTTTCKhoan.Checked = false;
+            }
+        }
+
+        // Sự kiện khi chọn Chuyển Khoản
+        private void checkBox_PTTTCKhoan_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_PTTTCKhoan.Checked)
+            {
+                checkBox_PTTTTienMat.Checked = false;
+            }
+        }
+        private void dateTimePicker_NgayNhan_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void dateTimePicker_ThgianNhan_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Them_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox_MaDDH.Text) ||
+                string.IsNullOrWhiteSpace(textBox_MaNV.Text) ||
+                string.IsNullOrWhiteSpace(textBox_MaNCC.Text) ||
+                string.IsNullOrWhiteSpace(textBox_MaLH.Text))
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Kiểm tra ngày nhận có nhỏ hơn hoặc bằng ngày hiện tại không
+            if (dateTimePicker_NgayNhan.Value.Date > DateTime.Now.Date)
+            {
+                MessageBox.Show("Ngày nhận phải nhỏ hơn hoặc bằng ngày hiện tại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Get payment method
+            string pttt = "";
+            if (checkBox_PTTTTienMat.Checked) pttt += "Tiền mặt ";
+            if (checkBox_PTTTCKhoan.Checked) pttt += "Chuyển khoản";
+            if (string.IsNullOrWhiteSpace(pttt))
+            {
+                MessageBox.Show("Vui lòng chọn phương thức thanh toán!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Create message with all information
+            // Thêm dữ liệu vào DataGridView
+            dataGridView_HoadonNhap.Rows.Add(
+                textBox_MaDDH.Text,
+                textBox_MaNV.Text,
+                textBox_MaNCC.Text,
+                textBox_MaLH.Text,
+                dateTimePicker_NgayNhan.Value.ToShortDateString(),
+                dateTimePicker_ThgianNhan.Value.ToLongTimeString(),
+                pttt
+            );
+            ClearForm();
+        }
+
+
+        private void dataGridView_HoadonNhap_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Lưu lại chỉ số hàng đang chọn
+                selectedRowIndex = e.RowIndex;
+
+                // Load dữ liệu lên form
+                textBox_MaDDH.Text = dataGridView_HoadonNhap.Rows[e.RowIndex].Cells[0].Value.ToString();
+                textBox_MaNV.Text = dataGridView_HoadonNhap.Rows[e.RowIndex].Cells[1].Value.ToString();
+                textBox_MaNCC.Text = dataGridView_HoadonNhap.Rows[e.RowIndex].Cells[2].Value.ToString();
+                textBox_MaLH.Text = dataGridView_HoadonNhap.Rows[e.RowIndex].Cells[3].Value.ToString();
+                dateTimePicker_NgayNhan.Value = DateTime.Parse(dataGridView_HoadonNhap.Rows[e.RowIndex].Cells[4].Value.ToString());
+                dateTimePicker_ThgianNhan.Value = DateTime.Parse(dataGridView_HoadonNhap.Rows[e.RowIndex].Cells[5].Value.ToString());
+
+                string pttt = dataGridView_HoadonNhap.Rows[e.RowIndex].Cells[6].Value.ToString();
+                checkBox_PTTTTienMat.Checked = pttt == "Tiền mặt";
+                checkBox_PTTTCKhoan.Checked = pttt == "Chuyển khoản";
+            }
+        }
+
+        private void button_Sua_Click(object sender, EventArgs e)
+        {
+            if (selectedRowIndex >= 0)
+            {
+                if (string.IsNullOrWhiteSpace(textBox_MaDDH.Text) ||
+                    string.IsNullOrWhiteSpace(textBox_MaNV.Text) ||
+                    string.IsNullOrWhiteSpace(textBox_MaNCC.Text) ||
+                    string.IsNullOrWhiteSpace(textBox_MaLH.Text))
+                {
+                    MessageBox.Show("Vui lòng điền đầy đủ thông tin!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                dataGridView_HoadonNhap.Rows[selectedRowIndex].Cells[0].Value = textBox_MaDDH.Text;
+                dataGridView_HoadonNhap.Rows[selectedRowIndex].Cells[1].Value = textBox_MaNV.Text;
+                dataGridView_HoadonNhap.Rows[selectedRowIndex].Cells[2].Value = textBox_MaNCC.Text;
+                dataGridView_HoadonNhap.Rows[selectedRowIndex].Cells[3].Value = textBox_MaLH.Text;
+                dataGridView_HoadonNhap.Rows[selectedRowIndex].Cells[4].Value = dateTimePicker_NgayNhan.Value.ToShortDateString();
+                dataGridView_HoadonNhap.Rows[selectedRowIndex].Cells[5].Value = dateTimePicker_ThgianNhan.Value.ToLongTimeString();
+                dataGridView_HoadonNhap.Rows[selectedRowIndex].Cells[6].Value = checkBox_PTTTTienMat.Checked ? "Tiền mặt" : "Chuyển khoản";
+
+                MessageBox.Show("Cập nhật thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearForm();
+            }
+        }
+
+        private void button_Xoa_Click(object sender, EventArgs e)
+        {
+            if (selectedRowIndex >= 0)
+            {
+                dataGridView_HoadonNhap.Rows.RemoveAt(selectedRowIndex);
+                ClearForm();
+            }
+        }
+        private void ClearForm()
+        {
+            textBox_MaDDH.Clear();
+            textBox_MaNV.Clear();
+            textBox_MaNCC.Clear();
+            textBox_MaLH.Clear();
+            dateTimePicker_NgayNhan.Value = DateTime.Now;
+            dateTimePicker_ThgianNhan.Value = DateTime.Now;
+            checkBox_PTTTTienMat.Checked = false;
+            checkBox_PTTTCKhoan.Checked = false;
+            selectedRowIndex = -1;
+        }
+    }
+}
